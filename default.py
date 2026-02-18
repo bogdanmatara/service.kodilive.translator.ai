20e9a9404bc4f5eda2f657fb03bb7073b9181b61
bogdanmatara <bogdan.matara@gmail.com>
2026-02-18 18:41:57 +0200

    Delete default.py

--- default.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-import xbmcaddon
-import xbmcgui
-
-addon = xbmcaddon.Addon()
-addon_name = addon.getAddonInfo('name')
-
-# If a user manually clicks the addon in the menu, show a small message
-xbmcgui.Dialog().ok(addon_name, "Acest serviciu rulează în fundal și traduce automat subtitrările în limba română.")
